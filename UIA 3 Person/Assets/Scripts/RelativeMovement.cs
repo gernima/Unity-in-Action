f0bfbe591c468723894789495b15763bec7d4a8b
Max <braer.maks@gmail.com>
2021-09-12 19:21:29 +0300

    complete chapter 9

--- UIA 3 Person/Assets/Scripts/RelativeMovement.cs
+++ UIA 3 Person/Assets/Scripts/RelativeMovement.cs
@@ -13,6 +13,7 @@ public class RelativeMovement : MonoBehaviour
     public float gravity = -9.8f;
     public float terminalVelocity = -10.0f;
     public float minFall = -1.5f;
+    public float pushForce = 3.0f;
 
     private float _vertSpeed;
 
@@ -95,5 +96,11 @@ public class RelativeMovement : MonoBehaviour
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         _contact = hit;
+
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body != null && !body.isKinematic)
+        {
+            body.velocity = hit.moveDirection * pushForce;
+        }
     }
 }
